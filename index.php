1bdaaf7a35dd7f381333caae5e40c6fed0752c3e
aimhighagency <bryan@aimhighagency.com>
2015-10-18 14:18:41 -0700

    inital commit

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+	<head>
+		<title></title>
+		
+		<link rel="stylesheet" href="css/main.css">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta property="og:title" content="" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="" />
+		<meta property="og:image" content="" />
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.js"></script>
+		<!-- <script type="text/javascript" src="js/instafeed.js"></script>-->
+		<!-- <script type="text/javascript" src="js/twitterfeed.js"></script>-->
+		<script type="text/javascript" src="js/init.js"></script>
+	</head>
+
+	<body>
+
+		<div class="feather"><img src="images/HB-feather.png" alt="" /></div>
+		<div class="trigger-feather"></div>
+
+		<div class="tl"><img src="images/HB-border-tl.png" alt="" /></div>
+		<div class="tr"><img src="images/HB-border-tr.png" alt="" /></div>
+
+		<div class="t"></div>
+		<div class="b"></div>
+
+		<div id="wrap">
+
+		<!-- 
+		
+		
+
+		
+		<div class="l"></div>
+		<div class="r"></div> -->
+
+
+			 
+			<div id="header">
+				<h1>The Haunted Ballet</h1>
+				<h2><a href="">at the barnacle</a></h2>
+			</div>
+
+			<div id="main">
+				
+				<div class="section">
+
+					<div class="inner">
+						<div class="at"><img src="images/HB-accent-t.png" alt="" /></div>
+						<p>Come meet the spirits! Bid welcome to those from a bygone
+era who haunt the lands of Coconut Grove. Watch as they
+dance their stories beneath the moonlit sky and try to 
+escape their tragic endings night after magical night.</p>
+						<div class="ab"><img src="images/HB-accent-b.png" alt="" /></div>
+					</div>
+
+				</div>
+
+				<div class="section">
+					<div class="inner">
+						<h1>October</h1>
+						<br />
+						<h3>28th and 29th</h3>
+						<h2>8:30 pm</h2>
+						<br />
+						<p>3485 Main Hwy, Miami, FL 33133 <a href="https://www.google.com/maps/place/3485+Main+Hwy,+Miami,+FL+33133/@25.725306,-80.2447137,17z/data=!4m7!1m4!3m3!1s0x88d9b7cfe2bb4459:0x9df133ef503f5b79!2s3485+Main+Hwy,+Miami,+FL+33133!3b1!3m1!1s0x88d9b7cfe2bb4459:0x9df133ef503f5b79" target="_blank">Map</a></p>
+						<br />
+						<p class="small">Admission on site, starting at $10</p>
+					</div>
+				</div>
+
+				<div class="section">
+
+					<div id="contact" class="inner">
+						<div class="at"><img src="images/HB-accent-t.png" alt="" /></div>
+						
+						<p><a href="mailto:info@thehauntedballet.com">info@TheHauntedBallet.com</a></p>
+
+						<div class="social-icons">
+							<a href="http://instagram.com/thehauntedballet" target="_blank" class="icon-instagram"></a>
+							<a href="https://www.facebook.com/events/907776655969538/" target="_blank" class="icon-facebook"></a>
+						</div>
+						<div class="ab"><img src="images/HB-accent-b.png" alt="" /></div>
+					</div>
+
+				</div>
+				
+			</div>
+				
+
+				<div id="footer">
+					
+					<div class="copy">
+						&copy; <?php echo date('Y'); ?> The Haunted Ballet &mdash; Don't Steal. It's not nice.
+					</div>
+				</div>
+
+			</div>
+			
+		</div>
+
+		<div class="bl"><img src="images/HB-border-bl.png" alt="" /></div>
+		<div class="br"><img src="images/HB-border-br.png" alt="" /></div>
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
+		<!-- <script type="text/javascript" src="js/instafeed.js"></script>-->
+		<!-- <script type="text/javascript" src="js/twitterfeed.js"></script>-->
+		<!-- <script type="text/javascript" src="js/init.js"></script>-->
+		<!-- <script type="text/javascript" src="js/init.js"></script>-->
+		<script type="text/javascript" src="js/backstretch.js"></script>
+		<script type="text/javascript" src="js/init.js"></script>
+
+	</body>
+
+</html>
